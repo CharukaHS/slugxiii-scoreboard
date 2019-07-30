4e1281a010d7d917af99287f8f4048306d37a0c3
CharukaHS <Charuka12@gmail.com>
2019-07-30 12:19:36 +0530

    Sidebar structure created :art:

---
+++ components/SideMenu/Menu.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container flex flex-col justify-center px-16">
+    <img src="/logo.png" class="mt-10 mb-2 px-4" />
+
+    <input
+      class="bg-white focus:outline-0 focus:shadow-outline border border-gray-300 rounded-full py-2 px-4 block w-full appearance-none leading-normal mb-20"
+      type="search"
+      placeholder="Search"
+    />
+
+      <Pill />
+      <Pill />
+      <Pill />
+      <Pill />
+      <Pill />
+      <Pill />
+  </div>
+</template>
+
+<script>
+import Pill from '~/components/SideMenu/Pill'
+
+export default {
+  components: {
+    Pill
+  }
+}
+</script>
+
+
---
+++ components/SideMenu/Pill.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-gray-300 border rounded-full py-2 px-4 my-2">
+    <p>Volleyball</p>
+  </div>
+</template>
